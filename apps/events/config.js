e29728d3af48cce582f66b7aba25f330531776bc
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2019-08-22 15:05:52 +0200

    renames events app to history

--- apps/events/config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  name: `${path.basename(__dirname)} app`, // replace with your applications human readable name
-  path: path.basename(__dirname),
-  controller: 'controller.js',
-  symbol: 'fas fa-circle' // use classes for Fontawesome fonts here ( https://fontawesome.com/icons?d=gallery&m=free )
-};
