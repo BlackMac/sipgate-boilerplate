a81e6d1df2f6d5f698227869d03ae07abbf52603
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2019-08-22 10:10:59 +0200

    new control panel design

--- controllers/overview.js
+++ controllers/overview.js
@@ -12,15 +12,24 @@ exports.getOverview = (req, res) => {
     sipgate.getBalance(tokenInfo),
     sipgate.getPhoneNumbers(tokenInfo),
     sipgate.blacklist.getIncoming(tokenInfo),
-    sipgate.getUsers(tokenInfo)
-  ]).then(([balance, phoneNumbers, blacklist, users]) => {
+    sipgate.getUsers(tokenInfo),
+    sipgate.getAddresses(tokenInfo),
+    sipgate.getGroups(tokenInfo)
+  ]).then(([balance, phoneNumbers, blacklist, users, addresses, groups]) => {
+    let unverifiedCount = 0;
+    addresses.forEach((address) => {
+      if (address.type !== 'VERIFIED') unverifiedCount++;
+    });
     res.render('overview', {
       title: 'Account Overview',
       unknownUser,
       phoneNumbers,
       blacklist,
+      unverifiedCount,
+      addresses,
       balance,
       users,
+      groups,
       intl: new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' })
     });
   }).catch((error) => {
