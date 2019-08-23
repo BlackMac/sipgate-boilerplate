b0242bd0d5a66a918d708deb3a33226d53056a30
Stefan Lange Hegermann <lange-hegermann@sipgate.de>
2019-08-23 14:09:35 +0200

    some more information in the direct access example

--- apps/scratchbook/samples/direct_api.js
+++ apps/scratchbook/samples/direct_api.js
@@ -1,2 +1,8 @@
 // Fetch user information and log result after promise resolution
-await sipgate.getRest('balance');
+// You can find documentation for the REST API here: https://api.sipgate.com/v2/doc
+// the following methods are available:
+// sipgate.getRest(path, filter)
+// sipgate.postRest(path, filter, params)
+// sipgate.putRest(path, filter, params)
+
+await sipgate.getRest('blacklist/incoming');
